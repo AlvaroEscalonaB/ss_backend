694b2a52c08f2354be339881afff88bfb9432eac
Alvaro Escalona <ajescalona@uc.cl>
2023-06-17 04:50:18 -0400

    Add cors and add enum for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :favorite_cat_facts
   has_many :cat_facts, through: :favorite_cat_facts
 
+  enum role {
+    'common': 0,
+    'admin': 1,
+  }
+
   def favorite_cat_facts
     self.cat_facts
   end
